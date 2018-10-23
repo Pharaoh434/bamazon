4125cfcbe918fbe356fa63212b0d5c0befe5fb1d
Anthony <tomas.anthony.toledo@gmail.com>
2018-10-22 19:23:21 -0500

    Stuff

---
+++ schema.sql
@@ -0,0 +1,14 @@
+USE bamazon;
+
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('apple', 'produce', '1.00', '200.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('banana', 'produce', '2.99', '500.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('carrot', 'produce', '1.99', '600.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('grape', 'produce', '3.99', '200.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('water', 'beverage', '4.99', '300.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('pizza', 'frozen', '6.99', '100.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('cheese', 'dairy', '7.99', '500.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('gum', 'candy', '2.00', '600.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('strawberry', 'produce', '2.50', '211.00');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('orange', 'produce', '2.75', '550.00');
+
+SELECT * FROM products;
